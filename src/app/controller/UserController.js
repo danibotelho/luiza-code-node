29caae562699a42bbb1fd3d43d2adeb7453c77ed
Dani Botelho <danibl26@gmail.com>
2021-08-28 15:44:00 -0300

    projeto luizacode

---
+++ src/app/controller/UserController.js
@@ -0,0 +1,45 @@
+import * as Yup from 'yup';
+import User from '../models/User'
+
+class UserController{
+  async store(req, res){
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if(!(await schema.isValid(req.body))){
+      return res.status(401).json({ message: 'Ooops dados inválidos' })
+    }
+
+    const userExists = await User.findOne({
+       where: { 
+         email: req.body.email 
+        }
+      });
+
+    if(userExists){
+      return res.status(401).json({ message: 'Usuário já cadastrado em nossa base' })
+    }
+
+    const { id, name, email } = await User.create(req.body);
+    return res.json({id, name, email});
+  };
+
+  async index(req, res){
+    const person = {
+      name: "Dani",
+      age: 21
+    }
+    return res.status(200).json(person);
+  };
+  async delete(req, res){
+    return res.status(200).json({ message: 'Isso aí psiti!'});
+  };
+  async update(req, res){
+    return res.status(200).json({ message: 'Isso aí psiti!'});
+  };
+}
+
+export default new UserController();
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'chunee.db.elephantsql.com',
+  username: 'ymxmchts',
+  password: '7IzxIc5QX9wN_SVAehEZKL8yQMfzyQwT',
+  database: 'ymxmchts',
+  define: {
+    timeStamps: true,
+    underscored: true,
+    underscoredAll: true,
+  }
+}
